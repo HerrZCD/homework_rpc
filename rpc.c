8799357d29648f6ec939536267b3bcce48f9e6cf
HerrZCD <352273172@qq.com>
2023-05-04 16:20:43 +0800

    Iniliaize server

--- rpc.c
+++ rpc.c
@@ -1,12 +1,60 @@
 #include "rpc.h"
 #include <stdlib.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 struct rpc_server {
     /* Add variable(s) for server state */
+    int server_fd;
+    struct sockaddr* socket_address;
+    socklen_t* address_len;
 };
 
 rpc_server *rpc_init_server(int port) {
-    return NULL;
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = { 0 };
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket failed");
+        return NULL;
+    }
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET,
+                SO_REUSEADDR, &opt,
+                sizeof(opt))) {
+        perror("setsockopt");
+        return NULL;
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr*)&address,
+            sizeof(address))
+        < 0) {
+        perror("bind failed");
+        return NULL;
+    }
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        return NULL;
+    }
+    rpc_server* server = (rpc_server*)malloc(sizeof(rpc_server));
+
+    server->address_len = (socklen_t*)sizeof(address);
+    server->socket_address = (struct sockaddr*)&address;
+    server->server_fd = server_fd;
+    return server;
+
 }
 
 int rpc_register(rpc_server *srv, char *name, rpc_handler handler) {
